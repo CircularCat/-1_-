0387ab03b6fc94d5b8c6d2f806921a338dee8f88
CircularCat <44330346+CircularCat@users.noreply.github.com>
2019-04-17 20:01:27 +0800

    Update source.c
    增加了函数6

--- source.c
+++ source.c
@@ -6,25 +6,25 @@
 #define MAX_LEN 50
 
 //定义结构体
-typedef struct {                                 //定义单个学生的结构体类型
-	int sno;										      //学号
-	char sname[MAX_LEN+1];				 //名字
-	char sex[MAX_LEN+1];						                  //性别
-	char major[MAX_LEN+1];                //专业
+typedef struct {//定义单个学生的结构体类型
+	int sno;	//学号
+	char sname[MAX_LEN + 1];	//名字
+	char sex[MAX_LEN + 1];//性别
+	char major[MAX_LEN + 1];//专业
 }S_stu;
-typedef struct stu_node {                     //定义学生结点类型struct_node这里首字母用小写和它的类型名区分开
+typedef struct stu_node {//定义学生结点类型struct_node这里首字母用小写和它的类型名区分开
 	S_stu data;
 	struct stu_node *next;
 }Stu_node;
-typedef struct stu_link {                   //定义链表结构
-	Stu_node *head;                                //头指针
-	int size;                                        //链表元素个数
+typedef struct stu_link {//定义链表结构
+	Stu_node *head;//头指针
+	int size;//链表元素个数
 }Stu_link;
 
-typedef struct {                                           //单个课程信息结构体
-	int cno;                                                  //课程号
-	char cname[MAX_LEN+1];                        //课程名称
-	int classHours;                                       //课时数
+typedef struct {//单个课程信息结构体
+	int cno;//课程号
+	char cname[MAX_LEN + 1];//课程名称
+	int classHours;//课时数
 }S_cou;
 typedef struct cou_node {
 	S_cou data;
@@ -35,10 +35,10 @@ typedef struct cou_link {
 	int size;
 }Cou_link;
 
-typedef struct {                                     //课程成绩信息结构体
-	int sno;                                                //学号
-	int cno;                                                //课程号
-	int score;                                             //成绩
+typedef struct {//课程成绩信息结构体
+	int sno;//学号
+	int cno;//课程号
+	int score;//成绩
 }S_cog;
 typedef struct cog_node {//课程成绩信息结点
 	S_cog data;
@@ -61,10 +61,13 @@ int init(void);//初始化链表（置空，加头结点）
 int choose(void);//选择功能
 int pri_stu(Stu_link *stus);//打印学生链表
 int pri_cou(Cou_link *cous);//打印课程链表
+int pri_cog(Cog_link *cogs);//打印课程成绩信息链表
 int sor_stu_sno(Stu_link *stus);//将学生链表根据学号排序，输入为链表结构地址
 int sor_cou_cno(Cou_link *cous);//课程信息排序
+int sor_cog_sno_cno(Cog_link *cogs);//课程成绩信息排序
 int fun4(void);//选4时
 int fun5(void);//选5时
+int fun6(void);//选6时
 
 
 //函数实现
@@ -139,33 +142,51 @@ int pri_cou(Cou_link *cous) {
 	return 1;
 }
 
+int pri_cog(Cog_link *cogs) {
+	Cog_node *p = cogs->head;
+	if (NULL == p) {
+		printf("链表故障\n");
+		return 0;
+	}
+	while (p->next != NULL) {
+		printf("学号:%d\t课程号%d\t成绩%d\n",
+			p->data.sno,
+			p->data.cno,
+			p->data.score);
+
+		p = p->next;
+	}
+
+	return 1;
+}
+
 int sor_stu_sno(Stu_link *stus) {
 	Stu_node *p = stus->head, *q = p->next;
 	if (NULL == p) {
 		printf("链表故障\n");
 		return 0;
 	}
-	if ((0 == stus->size) || (1 == stus->size)){//空链表或者只有一个元素
+	if ((0 == stus->size) || (1 == stus->size)) {//空链表或者只有一个元素
 		return 1;
-    }
+	}
 	//检测、交换
-		S_stu temp;
-		int i = 0, j = 0;
-		for (i = 0; i < stus->size - 1; i++) {
-			for (j = 0; j < stus->size - i - 1; j++) {
-				if (p->data.sno > q->data.sno) {
-					temp = q->data;
-					q->data = p->data;
-					p->data = temp;
-				}
-				p = q;
-				q = q->next;
+	S_stu temp;
+	int i = 0, j = 0;
+	for (i = 0; i < stus->size - 1; i++) {
+		for (j = 0; j < stus->size - i - 1; j++) {
+			if (p->data.sno > q->data.sno) {
+				temp = q->data;
+				q->data = p->data;
+				p->data = temp;
 			}
-			p = stus->head;
-			q = p->next;
+			p = q;
+			q = q->next;
 		}
-		
-	 
+		p = stus->head;
+		q = p->next;
+	}
+
+
 
 	return 1;
 }
@@ -197,6 +218,51 @@ int sor_cou_cno(Cou_link *cous) {
 	}
 
 
+
+	return 1;
+}
+
+int sor_cog_sno_cno(Cog_link *cogs) {
+	Cog_node *p = cogs->head, *q = p->next;
+	if (NULL == p) {
+		printf("链表故障\n");
+		return 0;
+	}
+	if ((0 == cogs->size) || (1 == cogs->size)) {//空链表或者只有一个元素
+		return 1;
+	}
+	//检测、交换
+	S_cog temp;
+	int i = 0, j = 0;
+	for (i = 0; i < cogs->size - 1; i++) {//课程号排序
+		for (j = 0; j < cogs->size - i - 1; j++) {
+			if (p->data.sno > q->data.sno) {
+				temp = q->data;
+				q->data = p->data;
+				p->data = temp;
+			}
+			p = q;
+			q = q->next;
+		}
+		p = cogs->head;
+		q = p->next;
+	}
+
+	for (i = 0; i < cogs->size - 1; i++) {//课程号排序
+		for (j = 0; j < cogs->size - i - 1; j++) {
+			if (p->data.sno == q->data.sno) {//学号相同则比较课程号大小
+				if (p->data.cno > q->data.cno) {
+					temp = q->data;
+					q->data = p->data;
+					p->data = temp;
+				}
+			}
+			p = q;
+			q = q->next;
+		}
+		p = cogs->head;
+		q = p->next;
+	}
 
 	return 1;
 }
@@ -250,35 +316,60 @@ int fun5(void) {
 	return 1;
 }
 
+int fun6(void) {
+	//读取文件并建立链表
+	FILE *fp = fopen("courseGrade.txt", "r");
+	if (NULL == fp) {//错误情况处理
+		printf("打开课程成绩信息文件失败\n");
+		return 0;
+	}
+	while (!feof(fp)) {
+		Cog_node *new_cog = (Cog_node*)malloc(sizeof(Cog_node));//新结点
+		memset(new_cog, 0, sizeof(Cog_node));
+		fscanf(fp, "%d", &new_cog->data.sno);
+		fscanf(fp, " \t%d", &new_cog->data.cno);
+		fscanf(fp, " \t%d\n", &new_cog->data.score);
+		new_cog->next = cogs.head;
+		cogs.head = new_cog;
+		cogs.size++;
+	}
+	printf("课程成绩信息链表已建立，共%d条记录\n", cogs.size);
+	//排序并输出
+	sor_cog_sno_cno(&cogs);
+	pri_cog(&cogs);
+
+	return 1;
+}
+
 int menu(void) {
-		printf("---------------------------------------------------------------\n");
-		printf("                         学生课程成绩查询系统\n");
+	printf("---------------------------------------------------------------\n");
+	printf("                         学生课程成绩查询系统\n");
 
-		printf("4.建立学生链表\n");
-		printf("5.建立课程链表\n");
-		//printf("6.建立成绩链表\n");
-		//printf("7.查询所有信息\n");
-		//printf("8.查询指定成绩\n");
-		//printf("9.小于60分学生\n");
-		//printf("10.逆序4的链表\n");
-		//printf("11.链式队列做7\n");
+	printf("4.建立学生链表\n");
+	printf("5.建立课程链表\n");
+	printf("6.建立成绩链表\n");
+	//printf("7.查询所有信息\n");
+	//printf("8.查询指定成绩\n");
+	//printf("9.小于60分学生\n");
+	//printf("10.逆序4的链表\n");
+	//printf("11.链式队列做7\n");
 
-		printf("0.退出系统并保存修改\n");
-		printf("-1.退出系统并不保存任何信息\n");
+	printf("0.退出系统并保存修改\n");
+	printf("-1.退出系统并不保存任何信息\n");
 
-		printf("---------------------------------------------------------------\n");
-		return 1;
+	printf("---------------------------------------------------------------\n");
+	return 1;
 }
 
 int main(void)
 {
-	                                          //变量声明
+	//变量声明
 	int cho = 0;
-	                                          //初始化
+	//初始化
 	init();
-	                                            //开始循环
+	//开始循环
 	while (1) {
-		                                            //打印菜单
+		//打印菜单
 		menu();
 		cho = choose();
 		//选择语句
@@ -296,13 +387,17 @@ int main(void)
 			fun5();
 			break;
 		}
+		if (6 == cho) {
+			fun6();
+			break;
+		}
 
-	                                                 //不保存退出
-	                                                 //保存退出
-	                                                 //其他函数实现
+		//不保存退出
+		//保存退出
+		//其他函数实现
 
 	}
-	
+
 	//结束循环
 
 	return 0;
